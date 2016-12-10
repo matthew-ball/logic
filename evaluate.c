67a44db8578bfe192edd8b03f03df4050206989a
Matthew Ball <mathew.ball@gmail.com>
2016-12-10 19:15:48 +1100

    Added De Morgan's (disjunction and conjunction) laws.

--- evaluate.c
+++ evaluate.c
@@ -2,8 +2,13 @@
 #include "environment.h"
 #include "evaluate.h"
 
+// https://en.wikipedia.org/wiki/Propositional_calculus#Basic_and_derived_argument_forms
 expression *simplify_expression(expression *exp) {
-  if (IS_NEGATION(exp) && IS_NEGATION(NEGATION(exp))) { /* double negation */
+  if (IS_NEGATION(exp) && IS_DISJUNCTION(NEGATION(exp))) { /* de morgan's disjunction */
+	return conjunction(negation(DISJUNCTION_LEFT(NEGATION(exp))), negation(DISJUNCTION_RIGHT(NEGATION(exp))));
+  } else if (IS_NEGATION(exp) && IS_CONJUNCTION(NEGATION(exp))) { /* de morgan's conjunction */
+	return disjunction(negation(CONJUNCTION_LEFT(NEGATION(exp))), negation(CONJUNCTION_RIGHT(NEGATION(exp))));
+  } else if (IS_NEGATION(exp) && IS_NEGATION(NEGATION(exp))) { /* double negation */
 	return simplify_expression(NEGATION(NEGATION(exp)));
   } else if (IS_CONJUNCTION(exp)) {
 	if (equal_expressions(simplify_expression(CONJUNCTION_LEFT(exp)), /* same conjuncts */
