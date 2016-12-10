d784a69f57c18b0a0677b219632ecabf52bb4d03
Matthew Ball <mathew.ball@gmail.com>
2016-12-10 17:20:53 +1100

    Cleaning up evaluation - implementing simplification.

--- evaluate.c
+++ evaluate.c
@@ -2,6 +2,10 @@
 #include "environment.h"
 #include "evaluate.h"
 
+expression *simplify_expression(expression *exp) {
+  return exp;
+}
+
 expression *evaluate_expression(expression *exp, environment *env) {
   add_to_environment(exp, &env);
 
